132ed2f0682145ee14bebb5a0ac3daa2f01ce46c
Hemma Ruhdorfer <RUH220437@spengergasse.at>
2026-06-24 10:46:00 +0200

    Test 10-CORRECT-Conjured Items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,47 +14,57 @@ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
+
   for (var i = 0; i < items.length; i++) {
+
+    var isConjured = items[i].name.indexOf("Conjured") === 0; //VON MIR
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+
+          items[i].quality = items[i].quality - (isConjured ? 2 : 1); //VIB MIR
+
         }
       }
     } else {
       if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
+        items[i].quality = items[i].quality + 1;
+
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = items[i].quality + 1;
             }
           }
           if (items[i].sell_in < 6) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = items[i].quality + 1;
             }
           }
         }
       }
     }
+
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
       items[i].sell_in = items[i].sell_in - 1;
     }
+
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+
+              items[i].quality = items[i].quality - 1;
+
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = items[i].quality - items[i].quality;
         }
       } else {
         if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+          items[i].quality = items[i].quality + 1;
         }
       }
     }
