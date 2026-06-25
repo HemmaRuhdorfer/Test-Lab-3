91a014e620613c37c89ea7e349ac7b2479862536
Hemma Ruhdorfer <RUH220437@spengergasse.at>
2026-06-25 16:55:10 +0200

    Revert "Test 10-CORRECT-Conjured Items"
    This reverts commit 132ed2f0682145ee14bebb5a0ac3daa2f01ce46c.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,8 +14,8 @@ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
-
   for (var i = 0; i < items.length; i++) {
+<<<<<<< HEAD
 
     var item = items[i];
 
@@ -81,6 +81,50 @@ function update_quality() {
 
     if (item.quality > 50) {
       item.quality = 50;
+=======
+    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (items[i].quality > 0) {
+        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          items[i].quality = items[i].quality - 1
+        }
+      }
+    } else {
+      if (items[i].quality < 50) {
+        items[i].quality = items[i].quality + 1
+        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (items[i].sell_in < 11) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1
+            }
+          }
+          if (items[i].sell_in < 6) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1
+            }
+          }
+        }
+      }
+    }
+    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      items[i].sell_in = items[i].sell_in - 1;
+    }
+    if (items[i].sell_in < 0) {
+      if (items[i].name != 'Aged Brie') {
+        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (items[i].quality > 0) {
+            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              items[i].quality = items[i].quality - 1
+            }
+          }
+        } else {
+          items[i].quality = items[i].quality - items[i].quality
+        }
+      } else {
+        if (items[i].quality < 50) {
+          items[i].quality = items[i].quality + 1
+        }
+      }
+>>>>>>> parent of 132ed2f (Test 10-CORRECT-Conjured Items)
     }
   }
 }
