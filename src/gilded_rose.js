6202ecdfa2ba8a029bd54b970600ff48fb0de3b0
Hemma Ruhdorfer <RUH220437@spengergasse.at>
2026-06-25 17:00:38 +0200

    FOUND PROBLEMS:Fixed Revert

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,77 +15,13 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-<<<<<<< HEAD
+    var isConjured = items[i].name.indexOf('Conjured') !== -1;
+    var decrement = isConjured ? 2 : 1;
 
-    var item = items[i];
-
-    var isAgedBrie = item.name === 'Aged Brie';
-    var isBackstage = item.name === 'Backstage passes to a TAFKAL80ETC concert';
-    var isSulfuras = item.name === 'Sulfuras, Hand of Ragnaros';
-    var isConjured = item.name.indexOf('Conjured') === 0;
-
-    // Sulfuras does not change
-    if (isSulfuras) {
-      continue;
-    }
-
-    // sell_in always decreases (except Sulfuras)
-    item.sell_in--;
-
-//aged brie check
-    if (isAgedBrie) {
-      if (item.quality < 50) {
-        item.quality++;
-      }
-      if (item.sell_in < 0 && item.quality < 50) {
-        item.quality++;
-      }
-    }
-//backstage pass check
-    else if (isBackstage) {
-
-      if (item.quality < 50) {
-        item.quality++;
-
-        if (item.sell_in < 10 && item.quality < 50) {
-          item.quality++;
-        }
-
-        if (item.sell_in < 5 && item.quality < 50) {
-          item.quality++;
-        }
-      }
-
-      if (item.sell_in < 0) {
-        item.quality = 0;
-      }
-    }
-
-//normale und conjured
-    else {
-
-      var decrease = isConjured ? 2 : 1;
-
-      if (item.quality > 0) {
-        item.quality -= decrease;
-      }
-
-      if (item.sell_in < 0 && item.quality > 0) {
-        item.quality -= decrease;
-      }
-    }
-//Cecks für quality
-    if (item.quality < 0) {
-      item.quality = 0;
-    }
-
-    if (item.quality > 50) {
-      item.quality = 50;
-=======
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+          items[i].quality = Math.max(0, items[i].quality - decrement)
         }
       }
     } else {
@@ -113,7 +49,7 @@ function update_quality() {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+              items[i].quality = Math.max(0, items[i].quality - decrement)
             }
           }
         } else {
@@ -124,7 +60,7 @@ function update_quality() {
           items[i].quality = items[i].quality + 1
         }
       }
->>>>>>> parent of 132ed2f (Test 10-CORRECT-Conjured Items)
+
     }
   }
 }
